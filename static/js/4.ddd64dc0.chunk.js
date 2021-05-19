b7371a871f65e1853f7b79e96a1c3043d18e30e5
Volodymyr Bondar <vova0737@gmail.com>
2021-05-19 03:53:33 +0300

    Updates

---
+++ static/js/4.ddd64dc0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw"]=this["webpackJsonpgoit-react-hw"]||[]).push([[4],{89:function(e,s,t){"use strict";t.r(s);t(0);var c=t(2);s.default=function(){return Object(c.jsxs)(c.Fragment,{children:[Object(c.jsx)("h1",{className:"Greeting",children:"Hello user"}),Object(c.jsx)("div",{className:"MessageContainer",children:Object(c.jsx)("h2",{children:"Log in, please"})})]})}}}]);
+//# sourceMappingURL=4.ddd64dc0.chunk.js.map
